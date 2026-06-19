5501031e6274b840115c50952a7c214d4c4a8ff2
akanasamuel <akanasamuel2@gmail.com>
2026-06-19 15:30:48 +0000

    Implement display wiring and tab switching functions
    Added functions for displaying wiring and tab switching.

--- Prototype/Src/main.js
+++ Prototype/Src/main.js
@@ -1,1 +1,29 @@
-
+//---------DISPLAY WIRING
+function displayWiring(){
+  document.querySelectorAll(".display-keys").forEach(btn=>{
+    btn.addEventListener("click",()=>{
+      document.getElementById("screen").value += btn.dataset.value
+    })
+  })
+}
+//---------TAB SWITCHING
+function tabSwitching(){
+  let currentTab = "main"
+  document.querySelectorAll(".inv").forEach(btn=>{
+    btn.addEventListener("click",()=>{
+      if(currentTab == "main"){
+        currentTab = "inv"
+        document.getElementById("right-keys").style.display = "none"
+        document.getElementById("inverse-keys").style.display = "grid"
+        return
+      }
+      else if(currentTab=="inv"){
+        currentTab = "main"
+        document.getElementById("inverse-keys").style.display = "none"
+        document.getElementById("right-keys").style.display = "grid"
+      }
+    })
+  })
+}
+tabSwitching()
+displayWiring()
